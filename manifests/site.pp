f72db709c3c98a86fecf1f8db74a81289c703f5f
root <root@puppet.codespaces.io>
2019-01-07 17:57:55 +0000

    Changed structure

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,5 @@
 #This is the main site.pp to hold node def but we will use hiera instead so tell hiera to include classes:
 #--------------------------------------------------------------------------------------------------------
-hiera_include('classes')
+node default {
+  hiera_include('classes')
+}
